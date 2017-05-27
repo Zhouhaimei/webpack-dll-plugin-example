b283308b57e161070dece69052a52fbf854efb07
ecoco <ecoco@twilio.com>
2017-05-26 17:06:52 -0700

    Add big dependencies and extra bundles to demo slow compile time.

--- src/index.jsx
+++ src/index.jsx
@@ -6,6 +6,21 @@ require("../styles/application.scss");
 // Render the top-level React component
 import React from 'react';
 import ReactDOM from 'react-dom';
+import jQuery from 'jquery';
+import d3 from 'd3';
+import highcharts from 'highcharts';
+import redux from 'redux';
+import bootstrap from 'bootstrap';
+import angular from 'angular';
 import App from './App.jsx';
+
+const useBigDependencies = () => {
+	d3.toString();
+	highcharts.toString();
+	redux.toString();
+	bootstrap.toString();
+	angular.toString();
+	jQuery.toString();
+};
 
 ReactDOM.render(<App />, document.getElementById('react-root'));
--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,21 @@ var webpack = require('webpack');
 
 module.exports = {
   devtool: 'eval',
-  entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    './src/index.jsx'
-  ],
+  entry: {
+    'webpack-dev-server/client?http://localhost:3000': './src/index.jsx',
+    'webpack-dev-server/clientTwo?http://localhost:3000': './src/index.jsx',
+    'webpack-dev-server/clientThree?http://localhost:3000': './src/index.jsx',
+    'webpack-dev-server/clientFour?http://localhost:3000': './src/index.jsx',
+    'webpack-dev-server/clientFive?http://localhost:3000': './src/index.jsx',
+    'webpack-dev-server/clientSix?http://localhost:3000': './src/index.jsx',
+    'webpack-dev-server/clientSeven?http://localhost:3000': './src/index.jsx',
+    'webpack-dev-server/clientEight?http://localhost:3000': './src/index.jsx',
+    'webpack-dev-server/clientNine?http://localhost:3000': './src/index.jsx',
+    'webpack-dev-server/clientTen?http://localhost:3000': './src/index.jsx'
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     publicPath: '/build/'
   },
   module: {
@@ -25,4 +33,4 @@ module.exports = {
       }
     ]
   }
-}
+};
